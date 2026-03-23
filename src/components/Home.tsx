bd7b5a9528c474ba38aa7565b3fbef6de5a8498a
니터로닝 <knitterronin@naver.com>
2026-03-23 15:28:53 +0900

    Ver1.0.0 - Home.tsx 작성

---
+++ src/components/Home.tsx
@@ -0,0 +1,74 @@
+// 
+import {useEffect, useState} from "react";
+
+export type MovieItem = {
+    "#TITLE": string;
+    "#YEAR": number;
+    "#IMDB_ID": string;
+    "#RANK": number;
+    "#ACTORS": string;
+    "#AKA": string;
+    "#IMDB_URL": string;
+    "#IMDB_IV": string;
+    "#IMG_POSTER": string;
+    photo_width: number;
+    photo_height: number;
+}
+
+const Home = () => {
+
+    // loading 상태관리에 대한 함수
+    const [ loading, setLoading ] = useState<boolean>(true);
+    //
+    const [ list, setList ] = useState<MovieItem[]>([]);
+
+    useEffect(() => {
+        fetch("https://imdb.iamidiotareyoutoo.com/search?q=2025&lsn=1&v=1")
+            .then(response => response.json())
+            .then(data => {
+                setList(data.description as MovieItem[]);
+                setLoading(false);
+            })
+    }, []);
+// 렌더링 파트(JSX & TSX = 태그를 사용할 수 있게해주는 구간)===================================================================
+    return (
+        <div>
+            {loading
+                ? <h1>loading...</h1>
+                : <div className={styles.container}>
+                    {list.map((item, index) => {
+                        return <li key={index}>{item}</li>
+                    })}
+
+                </div>
+
+
+
+
+            }
+        </div>
+    )
+}
+
+export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
